a515aaa2da1732ea5e68407e116a6e407f3434dc
Kate Sorotos <kate_sorotos@live.co.uk>
2018-12-07 11:30:21 +0000

    initial commit employee_project

---
+++ kate_pam_employee_project_test.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec  7 11:23:06 2018
+
+@author: Kate Sorotos
+"""
+
+from kate_pam_employee_project_function import * 
+
+dwight = Employee('dwight', 'schrute', 3, 10)
+michael = Employee_management('michael', 'scott', 1, 30, 1000) 
+
+#dwight.employee_info()
+#print('\n')
+#dwight.apply_for_leave_flow()
+#print('\n')
+#michael.claim_client_expenses()
